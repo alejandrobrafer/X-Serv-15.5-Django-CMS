83cfa87b8dc3bd3cea37b5370e256eb689312508
Alejandro  Bravo Fernández <alejandrobravofernandez@MacBook-Pro-de-Alejandro.local>
2018-04-02 16:07:54 +0200

    lunes,  2 de abril de 2018, 16:07:36

--- cms/views.py
+++ cms/views.py
@@ -11,5 +11,6 @@ def content(request, key):
 	try:
 		resource = Pages.objects.get(name=key)
 		return HttpResponse(resource.page)
+	
 	except Pages.DoesNotExist:
 		return HttpResponseNotFound('<h1><center>Resource not found</center></h1>')
