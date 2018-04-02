5bc4ebcb554f69d7864b8cd829fc387029b3d501
Alejandro  Bravo Fernández <alejandrobravofernandez@MacBook-Pro-de-Alejandro.local>
2018-04-02 15:54:54 +0200

    v1

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse, HttpResponseNotFound
+from cms.models import Pages
+
+
+def content(request, key):
+
+	try:
+		resource = Pages.objects.get(name=key)
+		return HttpResponse(resource.page)
+	except Pages.DoesNotExist:
+		return HttpResponseNotFound('<h1><center>Resource not found</center></h1>')
